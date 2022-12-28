20761086d8e7b65e15509c43a924c5bd8aa45b8b
botanist420 <lin36716@gmail.com>
2022-12-29 05:01:46 +0800

    functionalize nba

--- ds_lucas/nba.py
+++ ds_lucas/nba.py
@@ -1,29 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Module name: nba-api
 from nba_api.stats.endpoints import playercareerstats
-from nba_api.stats.static import teams
+# from nba_api.stats.static import teams
 from nba_api.stats.static import players
 
 
-# Challenge: find Curry id
-from nba_api.stats.static import players
-# get_players returns a list of dictionaries, each representing a player.
-nba_players = players.get_players()
-# print('Number of players fetched: {}'.format(len(nba_players)))
-nba_players[:5]
-nba_players_active = [player for player in nba_players if player.get("is_active") == True]
-curry_info = [i for i in nba_players_active if i.get("full_name") == "Stephen Curry"]
-curry_id = curry_info[0].get("id")
-# print(curry_id)
-# print(type(curry_id))
+def get_player_info(full_name: str = "Stephen Curry") -> pd.DataFrame:
+
+    nba_players = players.get_players()
+    nba_players_active = [
+        player for player in nba_players if player.get("is_active") == True]
+    curry_info = [i for i in nba_players_active if i.get(
+        "full_name") == full_name]
+    curry_id = curry_info[0].get("id")
+    career = playercareerstats.PlayerCareerStats(player_id=curry_id)
+    df_raw = career.get_data_frames()[0]
+    df_clean = df_raw[['SEASON_ID', 'PLAYER_AGE', 'GP', 'GS', 'MIN', 'FGM', 'FGA', 'FG3M', 'FG3A',
+                       'FTM', 'FTA', 'OREB', 'DREB', 'AST', 'STL',
+                       'BLK', 'TOV', 'PTS']]
+
+    return df_clean
 
 
-# Curry career data
-career = playercareerstats.PlayerCareerStats(player_id=curry_id)
-df_raw = career.get_data_frames()[0]
-print(df_raw.columns)
-df_clean = df_raw[['SEASON_ID', 'PLAYER_AGE', 'GP', 'GS', 'MIN', 'FGM', 'FGA', 'FG3M', 'FG3A',
-                   'FTM', 'FTA', 'OREB', 'DREB', 'AST', 'STL',
-                   'BLK', 'TOV', 'PTS']]
-
-
-
-print(df_clean.head())
+if __name__ == "__main__":
+    df = get_player_info()
+    print(df.head())
