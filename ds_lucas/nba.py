61db6da06fe5a6de9865e02ee57226af9006af0e
botanist420 <lin36716@gmail.com>
2022-12-29 05:21:09 +0800

    expand nba module

--- ds_lucas/nba.py
+++ ds_lucas/nba.py
@@ -6,8 +6,21 @@ import seaborn as sns
 
 # Module name: nba-api
 from nba_api.stats.endpoints import playercareerstats
-# from nba_api.stats.static import teams
+from nba_api.stats.static import teams
 from nba_api.stats.static import players
+
+
+def get_team_id(full_name: str = "Golden State Warriors") -> int:
+    # get_teams returns a list of 30 dictionaries, each an NBA team.
+    nba_teams = teams.get_teams()
+    # print('Number of teams fetched: {}'.format(len(nba_teams))) ## 沒意外會是30隊raw的資料
+    # print(nba_teams[:3])
+
+    # Challenge: find gsw
+    result = [i for i in nba_teams if i.get("full_name") == full_name][0]
+    team_id = result.get("id")
+    # print(type(team_id))
+    return team_id
 
 
 def get_player_info(full_name: str = "Stephen Curry") -> pd.DataFrame:
@@ -23,10 +36,10 @@ def get_player_info(full_name: str = "Stephen Curry") -> pd.DataFrame:
     df_clean = df_raw[['SEASON_ID', 'PLAYER_AGE', 'GP', 'GS', 'MIN', 'FGM', 'FGA', 'FG3M', 'FG3A',
                        'FTM', 'FTA', 'OREB', 'DREB', 'AST', 'STL',
                        'BLK', 'TOV', 'PTS']]
-
     return df_clean
 
 
 if __name__ == "__main__":
-    df = get_player_info()
-    print(df.head())
+    # df = get_player_info()
+    # print(df.head())
+    get_team_id()
